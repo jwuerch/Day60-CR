ab1c991f4867359bf833268c51bbe5fdfab74a42
Jason Wuerch <Jason Wuerch>
2016-03-25 10:01:56 -0700

    create new food component that allows you to input a string and create new food

---
+++ app/new-food.component.ts
@@ -0,0 +1,26 @@
+import {Component, EventEmitter} from 'angular2/core';
+import {Food} from './food';
+
+@Component({
+  selector: 'new-food',
+  outputs: ['onSubmitNewFood'],
+  template:
+  `
+  <div class="add-food">
+  <input class="col-sm-5" #newFood>
+  <button (click)="addFood(newFood)"></button>
+  </div>
+  `
+})
+
+export class AddFoodComponent {
+  public food: Food;
+  public onSubmitNewFood: EventEmitter<string>;
+  constructor() {
+    this.onSubmitNewFood = new EventEmitter();
+  }
+  addFood(foodName: HTMLInputElement) {
+    this.onSubmitNewFood.emit(foodName.value);
+    foodName.value = "";
+  }
+}
