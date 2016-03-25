de97ab6478e4d26856c99244d0ba55579e0ddd0c
Jason Wuerch <Jason Wuerch>
2016-03-25 09:45:37 -0700

    create initial files

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,20 +1,22 @@
-import {Component, EventEmitter} from 'angular2/core';
-import {Task} from './task';
-import {capitalize} from './task';
-import {NewTaskComponent} from './new-task.component';
-import {TaskListComponent} from './task-list.component';
-
-
-@Component({
-  selector: 'my-app',
-  directives: [NewTaskComponent, TaskListComponent],
-  template: `
-  <h1>hi</h1>
-  <task-list></task-list>
-
-  `
-})
-
-export class AppComponent {
-
-}
+// import {Component, EventEmitter} from 'angular2/core';
+// import {Food} from './food';
+// import {FoodListComponent} from './food-list.component';
+//
+//
+// @Component({
+//   selector: 'my-app',
+//   directives: [FoodListComponent],
+//   template:
+//   `
+//   <div class="container">
+//     <h1 class="text-primary">Welcome To Your Food App</h1>
+//     <br>
+//     <food-list></food-list>
+//   </div>
+//   `
+// })
+//
+//
+// export class AppComponent {
+//
+// }
